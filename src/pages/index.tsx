a4f072e6998b11d915d5024610aeab4f389c454b
JorgeRenan <jorgerenan94@gmail.com>
2026-03-12 20:41:19 -0300

    Utilização do token no front

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import Header from "@/components/Header";
 import Input from "@/components/ui/input";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home(){
+  const router = useRouter()
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+
+    if(!token){
+      router.push("/login")
+    }
+  }, [])
+  
   return (
     <div className="min-h-screen bg-[#121417]">
       <Header />
