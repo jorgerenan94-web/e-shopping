3819bc559586ef28432dc44e53065be23df40255
JorgeRenan <jorgerenan94@gmail.com>
2026-03-12 21:52:41 -0300

    Incrementação da requisição da api para o profile e a criação do pagewrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
 import Header from "@/components/Header";
 import Input from "@/components/ui/input";
+import requestApi from "@/helpers/requestApi";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 export default function Home(){
   const router = useRouter()
+  
+  async function getProfile(){
+    try {
+      await requestApi({
+        url: "/profile",
+        method: "GET"
+      })    
+    } catch (error) {
+      localStorage.removeItem("token")
+      router.push("/login")
+    }
+  }
 
   useEffect(() => {
     const token = localStorage.getItem("token")
@@ -12,8 +26,10 @@ export default function Home(){
     if(!token){
       router.push("/login")
     }
+
+    getProfile()
   }, [])
-  
+
   return (
     <div className="min-h-screen bg-[#121417]">
       <Header />
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,14 @@ const instance = axios.create({
     baseURL: "http://localhost:4505"
 });
 
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token")
+
+    if(token){
+        config.headers.Authorization = token
+    }
+
+    return config
+})
+
 export default instance;
