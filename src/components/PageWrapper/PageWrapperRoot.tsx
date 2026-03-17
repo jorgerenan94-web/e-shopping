82a2af147ec2e20fd3c2889abdb4e5314acb8595
JorgeRenan <jorgerenan94@gmail.com>
2026-03-17 18:59:17 -0300

    Incrementação do arquivo do partener do root e a criação do header, ambos relacionados ao page wrapper

--- src/components/PageWrapper/PageWrapperRoot.tsx
+++ src/components/PageWrapper/PageWrapperRoot.tsx
@@ -1,3 +1,13 @@
-export default function PageWrapper() {
-    
+interface PageWrapperRootProps {
+    children: React.ReactNode
+}
+
+export default function PageWrapper({ children }: PageWrapperRootProps) {
+    return (
+        <div className="min-h-screen bg-[#111418] flex">
+            <div className="flex-1">
+                {children}
+            </div>
+        </div>
+    )
 }
