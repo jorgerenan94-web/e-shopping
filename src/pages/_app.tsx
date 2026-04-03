4fd8fdd5cdcde9d21a566f1018773042d71fc135
JorgeRenan <jorgerenan94@gmail.com>
2026-04-02 22:11:05 -0300

    Implementação do Clarity para observação do projeto.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,18 @@ export default function App({ Component, pageProps }: AppProps) {
         position="top-right"
         autoClose={5000}
         theme="colored"
+      />
+      <Script 
+        id="clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "w5nt3n6r7c");`
+        }}  
       />
       <Component {...pageProps} />
     </>
